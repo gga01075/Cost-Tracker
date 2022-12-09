b79330caea980ac121c312ff356364843257019b
gga01075 <gga01075@naver.com>
2022-12-09 14:24:56 +0900

    비용데이터 추가 및 필터 적용중

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,30 @@
+import React, { useState } from 'react';
+
+
 import "./Expenses.css"
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020')
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    }
+
     return (
+        <div>
        <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onSelectYear={filterChangeHandler} />     
         { 
-            props.items.map((el,idx) => (
-                <ExpenseItem key={idx} date={el.date} title={el.title} amount={el.amount} />
-                )
-          ) 
+                props.items.map((el,idx) => (
+                    <ExpenseItem key={idx} date={el.date} title={el.title} amount={el.amount} />
+                    )
+            ) 
         }
         </Card>
-      
+        </div>
     )
 }
 
